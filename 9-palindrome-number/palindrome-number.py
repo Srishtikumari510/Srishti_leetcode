10e1f514e4227e7dd52d7814a21099130f197a77
SRISHTI KUMARI <125398898+Srishtikumari510@users.noreply.github.com>
2026-03-31 15:22:19 +0530

    Time: 8 ms (71.32%) | Memory: 12.5 MB (38.00%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPalindrome(self, x):
+       z=str(x)
+       y=z[::-1]
+
+       if z == y:
+          return True
+       else :
+            return False  
+
+
+        
