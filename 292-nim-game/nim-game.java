e876ec52b31a0d24ecf19062649c9f645843423b
SRISHTI KUMARI <125398898+Srishtikumari510@users.noreply.github.com>
2026-06-19 19:04:43 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (96.81%) - LeetSync

---
+++ 292-nim-game/nim-game.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canWinNim(int n) {
+        int x= n%4;
+
+        if(x==0)
+        {
+            return false;
+        }
+             return true;
+
+
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
